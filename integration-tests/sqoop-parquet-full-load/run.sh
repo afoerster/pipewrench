eafe78148e76917abcc53485009e713ceef64574
Tony Foerster <tony@phdata.io>
2018-03-14 15:23:17 -0500

    debug

--- integration-tests/sqoop-parquet-full-load/run.sh
+++ integration-tests/sqoop-parquet-full-load/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "original dir"
+pwd
 cd $SCRIPT_DIR
+echo "script dir"
+pwd
+ls -lah
 ./generate-scripts
 docker-compose exec kimpala /mount/sqoop-parquet-full-load/run-in-container.sh
